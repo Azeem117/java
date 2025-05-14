7e31f2e0def5b7d02db2d58d910113087d1a4960
Azeem117 <adwardcollin365@gmail.com>
2025-05-14 23:15:34 +0530

    hello

--- JAVA(2)/Cars.java
+++ JAVA(2)/Cars.java
@@ -50,7 +50,7 @@ public class Cars {
     }
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        
+        System.out.println("Hello");
     }
     
 }
