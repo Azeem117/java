9b34e5b88df9ffb1183ba4d431918c13b7a64a51
Azeem117 <adwardcollin365@gmail.com>
2025-05-25 18:47:26 +0530

    book

---
+++ JAVA(2)/Book.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+public class Book {
+    String Title;
+    String Author;
+    int isbn;
+    static int totalBook;
+    String nameOfBorrower;
+    public void borrowBook(String name ){
+        Scanner s = new Scanner(System.in);
+        this.nameOfBorrower = name;
+        int n;
+        System.out.print("Enter Number of books to be borrowed : ");
+        n = s.nextInt();
+        System.out.print("Book Have Been Given");
+        totalBook -= n;
+
+    }
+    public void returnBook(int noOfBookToReturn){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number of books to be returned : " );
+        noOfBookToReturn = sc.nextInt();
+        totalBook += noOfBookToReturn;
+        System.out.print("Books Have Been Returned");
+    }
+    public static void getTotalBooks(){
+        System.out.print("Total Number of Books In Library are : "+totalBook);
+
+    }
+    // public void searchBook(String Title,String Author){
+    //     System.out.print("isbn of the following book is "+isbn);
+    // }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
